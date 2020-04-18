bca1b44e137427fde53715bd8bbd53717ac65511
Manteliz <mantasvaist@gmail.com>
2020-04-18 19:52:18 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js';
+import { useState } from 'react';
 
 function App() {
+  const [peopleState, setPeopleState ] = useState({
+    people: [
+      { name: 'Mantas', age: 26 },
+      { name: 'Jonas', age: 27 },
+      { name: 'Shaun', age: 30 }
+    ]
+  });
+
+  const [otherState, setOtherState ] = useState('some othe value');
+
+  console.log(peopleState);
+
+  const switchState = () => {
+    setPeopleState({
+      people: [
+        { name: 'Mantaz', age: 26 },
+        { name: 'Jonaz', age: 27 },
+        { name: 'Shaunaz', age: 30 }
+      ]
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>cia yra React Hooks</h1>
+      <Person name={peopleState.people[0].name} age={peopleState.people[0].age} />
+      <Person name={peopleState.people[1].name} age={peopleState.people[1].age} />
+      <Person name={peopleState.people[2].name} age={peopleState.people[2].age} />
+      <button onClick={switchState}> Switch state </button>
     </div>
   );
 }
